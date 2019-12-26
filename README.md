0f3d3993276c8e1ab812f7b8b9d1dedf2d70a1f9
Ric <ricrogz@gmail.com>
2019-12-26 02:02:21 +0100

    first version

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # pdf_watermark
 A very simple tool to create and apply watermarks to pdf files using python
+
+# Usage
+
+1. Add the watermarks to the data file (`watermarks.csv` by default). Each watermark should be in a separate line and consist of exactly two fields separated by a comma. Lines with other than 2 fields will be skipped.
+2. Run the `create_watermarks.py` script. This will create the watermark pdfs in the `watermarks` directory. By default, watermark files will be named using the second field in the data file.
+3. Use `apply_watermarks.py [pdf to be watermarked]` to apply the watermarks. The watermark directory will be scanned for watermarks, and each of them will be used to generate a new watermarked copy of the input file, which will be placed inside the `output` directory.
+
+The two scripts are independent, so that watermarks can be added/replaced manually before application.
+
+The watermark data can be programatically modified before writing the watermark file. See the `transform_data()` function in the creation script.
+
+
---
+++ apply_watermarks.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python
+
+import glob
+import os
+import sys
+
+import PyPDF2
+
+watermarks_dir = './watermarks'
+output_dir = './output'
+
+
+def find_watermarks(watermarks_dir):
+    fname_expr = os.path.join(watermarks_dir, '*.[Pp][Dd][Ff]')
+    return glob.glob(fname_expr)
+
+
+def add_watermark(input_file, watermark_file, output_file):
+    """
+    Apply the watermarks page by page. We apply the content on top of
+    the watermark, because if we do it the other way, the watermark
+    might hide stuff. Unfortunately, this means that the watermark
+    gets modifed, and needs to be reloaded each time.
+    """
+
+    pdf_writer = PyPDF2.PdfFileWriter()
+
+    with open(output_file, "wb") as filehandle_output:
+        pdf = PyPDF2.PdfFileReader(input_file)
+        for page_num in range(pdf.numPages):
+            page = pdf.getPage(page_num)
+
+            wpdf = PyPDF2.PdfFileReader(watermark_file)
+            watermark = wpdf.getPage(0)
+            watermark.mergePage(page)
+            pdf_writer.addPage(watermark)
+
+        pdf_writer.write(filehandle_output)
+
+
+def main(input_file, watermarks_dir, output_dir):
+    if not os.path.exists(input_file):
+        print(f"""
+            Error: file '{input_file}' was not found.
+              """)
+        quit(1)
+
+    # Output file name is a combination of input and watermank file names
+    prefix = os.path.basename(input_file)[:-4]
+    for watermark_file in find_watermarks(watermarks_dir):
+        print(watermark_file)
+        suffix = os.path.basename(watermark_file)[:-4]
+        output_file = os.path.join(output_dir, f'{prefix}_{suffix}.pdf')
+
+        add_watermark(input_file, watermark_file, output_file)
+
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print("""
+            Usage:
+                apply_watermarks.py [pdf to be watermarked]
+            """)
+        quit(1)
+
+    main(sys.argv[1], watermarks_dir, output_dir)
