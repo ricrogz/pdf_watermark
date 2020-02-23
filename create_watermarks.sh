9161cf3bc8d5c708c87b6852a4467a41e7a99aac
Ric <ricrogz@gmail.com>
2020-02-23 12:23:59 -0500

    auto font size; discard blank input

--- create_watermarks.sh
+++ create_watermarks.sh
@@ -5,38 +5,46 @@ set -euo pipefail
 data_file='watermarks.csv'
 watermarks_dir='./watermarks'
 
-
 function trim_whitespace() {
     sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//' <<<"$1"
 }
 
-function create_watermark() {
-
-convert \
-    -density 144 \
-    -size 1190x1684 xc:transparent \
-    -fill black \
-    -pointsize 35 -weight 800 \
-    -gravity center \
-    -annotate -55,-55,20,50 "$1\n$2" \
-    miff:- | \
-composite \
-    -dissolve '30%' - \
-    -size 1190x1684 xc:transparent \
-    -page A4 \
-    -compress Zip \
-    "$3/$2.pdf"
+function get_font_size() {
+    bc <<<"v=(1000 - 15 * $1) / 45 * 5; if (v < 30) { 30 } else { v }"
 }
 
+function create_watermark() {
+
+    sz=$(get_font_size ${#1})
+    echo "sz=$sz"
+
+    convert \
+        -density 144 \
+        -size 1190x1684 xc:transparent \
+        -fill black \
+        -pointsize $sz -weight 800 \
+        -gravity center \
+        -annotate -55,-55,20,50 "$1\n$2" \
+        miff:- | \
+    composite \
+        -dissolve '30%' - \
+        -size 1190x1684 xc:transparent \
+        -page A4 \
+        -compress Zip \
+        "$3/$2.pdf"
+}
 
 function main() {
 
     while IFS=, read -r field1 field2
     do
-        echo "${field1}, ${field2}"
         field1=$(trim_whitespace "$field1")
         field2=$(trim_whitespace "$field2")
-        create_watermark "$field1" "$field2" $2
+        if [[ "" != "$field1" && "" != "$field2" ]]
+        then
+            echo "${field1}, ${field2}"
+            create_watermark "$field1" "$field2" $2
+        fi
     done < "$1"
 
 }
